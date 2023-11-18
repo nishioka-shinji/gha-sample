3be5e6493ef68c44eb56b3998faba3ccd16f48f5
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-11-18 20:20:09 +0900

    test

--- .github/scripts/create_pr.sh
+++ .github/scripts/create_pr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 BASE_REPO="$1"
 NEW_REPO="${BASE_REPO}_schemafile"
@@ -13,26 +14,16 @@ EOF
 git fetch
 git checkout $BASE_REPO
 
-if [ $? -ne 0 ]; then
-    echo "リポジトリ $BASE_REPO チェックアウト時にエラーが発生しました"
-    exit 1
-fi
-
 cat db/Schemafile > db/Schemafile_backup
 
 git checkout main
 git pull
 git checkout -b $NEW_REPO
 
-if [ $? -ne 0 ]; then
-    echo "リポジトリ $NEW_REPO 作成時にエラーが発生しました"
-    exit 1
-fi
-
 cat db/Schemafile_backup > db/Schemafile
 
-# git status を実行し、db/Schemafile の状態を確認
-STATUS=$(git status db/Schemafile --porcelain)
+# db/Schemafile の状態を確認
+STATUS=$(git diff --quiet db/Schemafile)
 
 if [ -z "$STATUS" ]; then
     echo "db/Schemafile に変更はありません。処理を中断します。"
@@ -40,12 +31,6 @@ if [ -z "$STATUS" ]; then
 fi
 
 git add db/Schemafile
-
-if [ $? -ne 0 ]; then
-    echo "db/Schemafileの追加時にエラーが発生しました"
-    exit 1
-fi
-
 git config --global user.name 'github-actions[bot]'
 git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git commit -m "Schemafile切り出し"
